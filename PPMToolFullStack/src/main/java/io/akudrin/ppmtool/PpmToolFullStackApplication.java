b2a078caaed423e7322b33edfd7a8967cd40025c
ak <andreikudrin@Andreis-MBP.connect>
2018-11-28 13:30:54 +0000

    first commit

---
+++ PPMToolFullStack/src/main/java/io/akudrin/ppmtool/PpmToolFullStackApplication.java
@@ -0,0 +1,12 @@
+package io.akudrin.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolFullStackApplication.class, args);
+	}
+}
