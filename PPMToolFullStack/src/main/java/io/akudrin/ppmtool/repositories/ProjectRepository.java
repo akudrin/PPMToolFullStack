06a466b0a3ce91f88a17d3a8fe7694005a38df90
ak <andreikudrin@Andreis-MBP.connect>
2018-11-28 14:35:54 +0000

    project object and repo

---
+++ PPMToolFullStack/src/main/java/io/akudrin/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package io.akudrin.ppmtool.repositories;
+
+import io.akudrin.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
