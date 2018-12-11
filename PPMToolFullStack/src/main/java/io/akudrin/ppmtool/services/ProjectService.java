53d796df7a38937fab967bde1ead63eb9184c4e3
Andrei Kudrin <andreikudrin@gmail.com>
2018-12-11 12:27:52 +0000

    custom exception

--- PPMToolFullStack/src/main/java/io/akudrin/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/akudrin/ppmtool/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.akudrin.ppmtool.domain.Project;
+import io.akudrin.ppmtool.exceptions.ProjectIdException;
 import io.akudrin.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -14,9 +15,12 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project){
 
-        //Logic
-
-        return projectRepository.save(project);
+    	try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+}
     }
 
 	
