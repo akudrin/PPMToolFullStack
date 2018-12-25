282aabe39500f774afd76642dc826c017a35ad3d
Andrei Kudrin <andreikudrin@gmail.com>
2018-12-25 08:27:56 +0000

    createproject

--- PPMToolFullStack/src/main/java/io/akudrin/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/akudrin/ppmtool/web/ProjectController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import io.akudrin.ppmtool.domain.Project;
 import io.akudrin.ppmtool.services.MapValidationErrorService;
 import io.akudrin.ppmtool.services.ProjectService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
