a229b1cde49878a13cf4ab5f0e32ff1fe80c6b04
Andrei Kudrin <andreikudrin@gmail.com>
2019-01-08 12:10:40 +0000

    get project backlog

--- PPMToolFullStack/src/main/java/io/akudrin/ppmtool/repositories/ProjectTaskRepository.java
+++ PPMToolFullStack/src/main/java/io/akudrin/ppmtool/repositories/ProjectTaskRepository.java
@@ -1,5 +1,7 @@
 package io.akudrin.ppmtool.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import io.akudrin.ppmtool.domain.ProjectTask;
 
 @Repository
 public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+	 List<ProjectTask> findByProjectIdentifierOrderByPriority(String id);
 }
