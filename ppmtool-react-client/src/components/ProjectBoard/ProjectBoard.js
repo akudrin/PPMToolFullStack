5fda0fd51521f426769146dd38aa8d1929b73f12
Andrei Kudrin <andreikudrin@gmail.com>
2019-01-25 12:14:08 +0000

    display tasks step1

--- ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
@@ -15,6 +15,7 @@ class ProjectBoard extends Component {
 
   render() {
     const { id } = this.props.match.params;
+    const { project_tasks } = this.props.backlog;
     return (
       <div className="container">
         <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
@@ -23,6 +24,7 @@ class ProjectBoard extends Component {
         <br />
         <hr />
         <Backlog />
+        <Backlog project_tasks_prop={project_tasks} />
       </div>
     );
   }
