e286b953b2f94a2b1fa546167dee7aa54a9ed27f
Andrei Kudrin <andreikudrin@gmail.com>
2019-02-19 08:19:41 +0000

    user already exists exception

--- PPMToolFullStack/src/main/java/io/akudrin/ppmtool/services/UserService.java
+++ PPMToolFullStack/src/main/java/io/akudrin/ppmtool/services/UserService.java
@@ -5,27 +5,32 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import io.akudrin.ppmtool.domain.User;
+import io.akudrin.ppmtool.exceptions.UsernameAlreadyExistsException;
 import io.akudrin.ppmtool.repositories.UserRepository;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public User saveUser (User newUser){
-      newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+	public User saveUser(User newUser) {
 
-      //Username has to be unique (exception)
+		try {
+			newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+			// Username has to be unique (exception)
+			newUser.setUsername(newUser.getUsername());
+			// Make sure that password and confirmPassword match
+			// We don't persist or show the confirmPassword
+			newUser.setConfirmPassword("");
+			return userRepository.save(newUser);
 
-        // Make sure that password and confirmPassword match
-        // We don't persist or show the confirmPassword
-      return userRepository.save(newUser);
-    }
+		} catch (Exception e) {
+			throw new UsernameAlreadyExistsException("Username '" + newUser.getUsername() + "' already exists");
+		}
 
-
-
+	}
 }
