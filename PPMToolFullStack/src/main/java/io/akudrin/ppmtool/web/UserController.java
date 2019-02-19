ce45654c256e7a4ae3ffc401081dc61037b8065e
Andrei Kudrin <andreikudrin@gmail.com>
2019-02-19 10:34:33 +0000

    user registration

--- PPMToolFullStack/src/main/java/io/akudrin/ppmtool/web/UserController.java
+++ PPMToolFullStack/src/main/java/io/akudrin/ppmtool/web/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import io.akudrin.ppmtool.domain.User;
 import io.akudrin.ppmtool.services.MapValidationErrorService;
 import io.akudrin.ppmtool.services.UserService;
+import io.akudrin.ppmtool.validator.UserValidator;
 
 @RestController
 @RequestMapping("/api/users")
@@ -24,10 +25,14 @@ public class UserController {
 
 	    @Autowired
 	    private UserService userService;
+	    
+	    @Autowired
+	    private UserValidator userValidator;
 
 	    @PostMapping("/register")
 	    public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
 	        // Validate passwords match
+	    	 userValidator.validate(user,result);
 
 	        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
 	        if(errorMap != null)return errorMap;
